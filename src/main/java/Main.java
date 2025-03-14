3d7fcbd8926b8ee7ae96cbcfb021859c5679a70f
Jason Fuchs <jasonfuchs@protonmail.com>
2025-03-14 12:12:35 +0100

    add support parsing JSON strings

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,7 +2,7 @@ import json.JsonParser;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-	var jsonValue = JsonParser.jsonValue().parse("42").orThrow();
+	var jsonValue = JsonParser.jsonValue().parse("42\"").orThrow();
 	System.out.println(jsonValue);
     }
 }
